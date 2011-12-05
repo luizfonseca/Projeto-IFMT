1ad48d98df3ad1d823198b73aa9892fdd92e7186
Dev <dev@dev-VirtualBox.(none)>
2011-12-05 09:59:13 -0300

    Teste das definitions

--- definitions/jarlan_math_def.rb
+++ definitions/jarlan_math_def.rb
@@ -13,3 +13,13 @@ end
 def divide(valor1, valor2)
 	return valor1 / valor2
 end
+
+puts "Digite o primeiro valor: "
+valor1 = gets.to_i
+puts "Digite o segundo valor: "
+valor2 = gets.to_i
+
+puts "Resultado da soma:  #{soma(valor1, valor2)}"
+puts "Resultado da subtracao:  #{subtrai(valor1, valor2)}"
+puts "Resultado da multiplicacao:  #{multiplica(valor1, valor2)}"
+puts "Resultado da divisao:  #{divide(valor1, valor2)}"
