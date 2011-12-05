ae6889fb0c465b47683ee37011591c1546986076
Dev <dev@dev-VirtualBox.(none)>
2011-12-05 08:51:18 -0300

    Coloquei um condicional no programa.

--- loops/jarlan_teste_loop.rb
+++ loops/jarlan_teste_loop.rb
@@ -1,10 +1,20 @@
 var = [1,2,3,4,5,6]
 var2 = 1.upto(10).to_a
 
+puts 'Variavel var:'
 for i in var
-	puts 'var = %d' % i
+	if i == 1
+		puts "Esse aqui e o numero que eu quero, o numero %d" % i
+	else
+		puts "Esse nao e o numero que eu quero, esse e o numero %d" % i
+	end
 end
 
+puts 'Variavel var2:'
 for i in var2
-	puts 'var2 = %d' % i
+	if i == 1
+		puts "Esse aqui e o numero que eu quero, o numero %d" % i
+	else
+		puts "Esse nao e o numero que eu quero, esse e o numero %d" % i
+	end
 end
