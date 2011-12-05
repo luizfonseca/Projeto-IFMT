54a44f8c37e396c525b990758b1905c2a80d2ec2
Luiz Fonseca <runeroniek@gmail.com>
2011-12-05 09:25:25 -0300

    Definitions test

---
+++ definitions/math_def.rb
@@ -0,0 +1,17 @@
+def soma(valor1, valor2)
+  return valor1 + valor2
+end
+
+def subtrai(valor1, valor2)
+  return valor1 - valor2
+end
+
+def multiplica(valor1, valor2)
+  return valor1 * valor2
+end
+
+def divide(valor1, valor2)
+  return valor1 / valor2
+end
+
+
