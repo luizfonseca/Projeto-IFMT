97e6c3bb8dcca3699424845a7f236d3c6da8fa69
Dev <dev@dev-VirtualBox.(none)>
2011-12-05 09:04:12 -0300

    Código Corrigido

---
+++ loops/Raylander_test_loop.rb
@@ -0,0 +1,14 @@
+var = [1,2,3,4,5,6]
+var = 1.upto(10).to_a
+
+for item in var
+	if item == 2
+		puts item
+	else
+		puts "Ops, Nao e' o numero que eu quero"
+	end
+end
+
+for i in var2
+	puts i
+end
