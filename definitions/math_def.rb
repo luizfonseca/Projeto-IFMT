9d3de5bc8496cdd8b16fbad3b5caa92cbbafda88
Luiz Fonseca <runeroniek@gmail.com>
2011-12-05 10:30:00 -0300

    Sample

--- definitions/math_def.rb
+++ definitions/math_def.rb
@@ -14,4 +14,12 @@ def divide(valor1, valor2)
   return valor1 / valor2
 end
 
+puts "Por favor, digite um numero:"
+valor1 = gets
+puts "Digite outro numero:"
+valor2 = gets
 
+puts "A soma dos dois numeros eh #{soma(valor1,valor2)}"
+puts "A soma dos dois numeros eh #{subtrai(valor1,valor2)}"
+puts "A soma dos dois numeros eh #{multiplica(valor1,valor2)}"
+puts "A soma dos dois numeros eh #{divide(valor1,valor2)}"
